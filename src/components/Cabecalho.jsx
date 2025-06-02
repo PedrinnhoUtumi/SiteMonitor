8804dbec1dc2ccad88325e95de744f744e2d5a9f
PedrinnhoUtumi <brunopena454@gmail.com>
2025-06-02 14:16:42 -0300

    Update Cabecalho.jsx

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -27,30 +27,30 @@
 // }
 
 
-// import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
-// export function Cabecalho() {
-//     const estiloBotao = "flex-1 min-w-[150px] px-6 py-3 bg-fundo_azul_escuro_elegante rounded-md text-branco hover:bg-azul_bebe hover:text-fundo_azul_escuro_elegante flex justify-center items-center text-base font-medium whitespace-nowrap"
+export function Cabecalho() {
+    const estiloBotao = "flex-1 min-w-[150px] px-6 py-3 bg-fundo_azul_escuro_elegante rounded-md text-branco hover:bg-azul_bebe hover:text-fundo_azul_escuro_elegante flex justify-center items-center text-base font-medium whitespace-nowrap"
 
-//     return (
-//         <div className="bg-fundo_azul_claro_elegante w-full h-24 flex items-center">
-//             <nav className="flex w-full max-w-screen-xl mx-auto px-4 gap-6">
-//                 <NavLink to="/TempoReal" className={estiloBotao}>
-//                     Tempo Real
-//                 </NavLink>
+    return (
+        <div className="bg-fundo_azul_claro_elegante w-full h-24 flex items-center">
+            <nav className="flex w-full max-w-screen-xl mx-auto px-4 gap-6">
+                <NavLink to="/TempoReal" className={estiloBotao}>
+                    Tempo Real
+                </NavLink>
 
-//                 <NavLink to="/Tecnico" className={estiloBotao}>
-//                     Técnico
-//                 </NavLink>
+                <NavLink to="/Tecnico" className={estiloBotao}>
+                    Técnico
+                </NavLink>
 
-//                 <NavLink to="/Relatorio" className={estiloBotao}>
-//                     Relatório
-//                 </NavLink>
+                <NavLink to="/Relatorio" className={estiloBotao}>
+                    Relatório
+                </NavLink>
 
-//                 <NavLink to="/Configuracao" className={estiloBotao}>
-//                     Configurações
-//                 </NavLink>
-//             </nav>
-//         </div>
-//     )
-// }
+                <NavLink to="/Configuracao" className={estiloBotao}>
+                    Configurações
+                </NavLink>
+            </nav>
+        </div>
+    )
+}
