7c9276f69b15aaa931430e0ae358c417c8336085
PedrinnhoUtumi <130059527+PedrinnhoUtumi@users.noreply.github.com>
2025-06-09 14:28:54 -0300

    Update DataContext.jsx

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,100 +1,3 @@
-// import { createContext, useContext, useEffect, useState } from "react";
-
-// export const DataContext = createContext();
-
-// export function DataProvider({ children }) {
-//   const [data, setData] = useState([]);
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [instituicao, setInstituicao] = useState([]);
-//   const [cargo, setCargo] = useState("");
-
-//   function adicionarDados(novosDados) {
-//     setData((prev) => {
-//       const existentes = new Set(prev.map((item) => JSON.stringify(item)));
-//       const novosUnicos = novosDados.filter(
-//         (item) => !existentes.has(JSON.stringify(item))
-//       );
-//       const atualizados = [...prev, ...novosUnicos];
-//       return atualizados;
-//     });
-//   }
-//   const adicionarNomes = (novoNome) => {
-//     setName(novoNome);
-//   };
-//   const adicionarEmail = (novoEmail) => {
-//     setEmail(novoEmail);
-//   };
-//   const adicionarInstituicao = (novaInstituicao) => {
-//     setInstituicao((prev) => {
-//       if (!prev.includes(novaInstituicao)) {
-//         return [...prev, novaInstituicao];
-//       }
-//       return prev;
-//     });
-//   };
-
-//   const adicionarCargo = (novoCargo) => {
-//     setCargo(novoCargo);
-//   }
-
-
-
-//   useEffect(() => {
-//     const fetchMachbase = async () => {
-//       try {
-//         const response = await fetch("http://127.0.0.1:3000/api");
-//         if (!response.ok) {
-//           throw new Error("Erro ao buscar os dados do servidor");
-//         }
-
-//         const json = await response.json();
-//         const tabelas = json.message;
-
-
-//         Object.entries(tabelas).forEach(([nomeTabela, conteudo]) => {
-//           const { columns, rows } = conteudo.data;
-
-//           if (!columns || columns.length === 0) {
-//             return;
-//           }
-
-//           const dadosFormatados = rows.map((linha) => {
-//             const obj = { __tabela: nomeTabela };
-//             columns.forEach((coluna, index) => {
-//               obj[coluna] = linha[index];
-//             });
-//             return obj;
-//           });
-
-//           adicionarDados(dadosFormatados);
-//         });
-//       } catch (err) {
-//         console.error("❌ Erro na requisição:", err.message);
-//       }
-//     };
-
-//     fetchMachbase();
-//   }, []);
-
-//   const exportar = {
-//     data,
-//     adicionarDados,
-//     name, 
-//     adicionarNomes,
-//     email,
-//     adicionarEmail,
-//     instituicao,
-//     adicionarInstituicao,
-//     cargo,
-//     adicionarCargo,
-
-//   };
-
-//   return (
-//     <DataContext.Provider value={exportar}>{children}</DataContext.Provider>
-//   );
-// }
 import { createContext, useContext, useEffect, useState } from "react";
 
 export const DataContext = createContext();
@@ -191,5 +94,6 @@ export function DataProvider({ children }) {
   };
 
   return (
-    { children });
+    <DataContext.Provider value={exportar}>{children}</DataContext.Provider>
+  );
 }
