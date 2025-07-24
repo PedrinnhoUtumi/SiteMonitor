d10cf285b8073aba4231c53a832c4a9fddcfcf66
“Bellin1” <130068559+brunpena@users.noreply.github.com>
2025-07-24 08:52:01 -0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
   
   const verificarLogin = async () => {
   try {
-    const response = await fetch("http://192.168.10.250:3000/api/login", {
+    const response = await fetch("http://192.168.3.250:3000/api/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
